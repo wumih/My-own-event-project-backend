4ea1b50c065d53ef3c8f61e7e175a7a039768546
wumih <120448608+wumih@users.noreply.github.com>
2025-11-14 00:42:07 +0800

    完成了登录部分的代码编写

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // 导入 express 模块
-const express = require('express')
+const express = require("express");
 // 创建 express 的服务器实例
-const app = express()
+const app = express();
 // 导入 cors 中间件
-const cors = require('cors')
+const joi = require("@hapi/joi");
+
+const cors = require("cors");
 // 将 cors 注册为全局中间件
-app.use(cors())
+app.use(cors());
 
 // 配置解析 application/x-www-form-urlencoded 格式的表单数据的中间件
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 // 响应数据的中间件 (一定要在路由之前)
 app.use(function (req, res, next) {
@@ -19,17 +21,24 @@ app.use(function (req, res, next) {
       status,
       // 状态描述，判断 err 是 错误对象 还是 字符串
       message: err instanceof Error ? err.message : err,
-    })
-  }
-  next()
-})
+    });
+  };
+  next();
+});
 
 // 导入并注册用户路由模块
-const userRouter = require('./router/user')
-app.use('/api', userRouter)
-// write your code here...
-app.use(express.urlencoded({ extended: false }))
+const userRouter = require("./router/user");
+app.use("/api", userRouter);
+
+// 错误中间件
+app.use(function (err, req, res, next) {
+  // 数据验证失败
+  if (err instanceof joi.ValidationError) return res.cc(err);
+  // 未知错误
+  res.cc(err);
+});
+
 // 调用 app.listen 方法，指定端口号并启动web服务器
 app.listen(3007, function () {
-  console.log('api server running at http://127.0.0.1:3007')
-})
+  console.log("api server running at http://127.0.0.1:3007");
+});
--- router_handler/user.js
+++ router_handler/user.js
@@ -3,6 +3,10 @@
  */
 // 导入数据库操作模块
 const db = require("../db/index");
+// 用这个包来生成 Token 字符串
+const jwt = require('jsonwebtoken')
+// 导入配置文件
+const config = require('../config')
 // 导入 bcryptjs 这个包
 const bcrypt = require("bcryptjs");
 // 注册用户的处理函数
@@ -54,11 +58,40 @@ exports.regUser = (req, res) => {
 
 // 登录的处理函数
 exports.login = (req, res) => {
-  res.send("login OK");
   // 接收表单数据
   const userinfo = req.body;
-  // 判断数据是否合法
-  if (!userinfo.username || !userinfo.password) {
-    return res.send({ status: 1, message: "用户名或密码不能为空！" });
-  }
+  // 定义 SQL 语句
+  const sql = `select * from ev_users where username=?`;
+  // 执行 SQL 语句，查询用户的数据
+  db.query(sql, userinfo.username, function (err, results) {
+    // 执行 SQL 语句失败
+    if (err) return res.cc(err);
+    // 执行 SQL 语句成功，但是查询到数据条数不等于 1
+    if (results.length !== 1) return res.cc("登录失败！");
+    // TODO：判断用户输入的登录密码是否和数据库中的密码一致
+    // 拿着用户输入的密码,和数据库中存储的密码进行对比
+    const compareResult = bcrypt.compareSync(
+      userinfo.password,
+      results[0].password
+    );
+
+    // 如果对比的结果等于 false, 则证明用户输入的密码错误
+    if (!compareResult) {
+      return res.cc("登录失败！");
+    }
+    // TODO：在服务器端生成 Token 的字符串
+    const user = { ...results[0], password: "", user_pic: "" };
+    // 生成 Token 字符串
+    const tokenStr = jwt.sign(user, config.jwtSecretKey, {
+      expiresIn: "10h", // token 有效期为 10 个小时
+    });
+
+    // TODO：登录成功，生成 Token 字符串
+    res.send({
+      status: 0,
+      message: "登录成功！",
+      // 为了方便客户端使用 Token，在服务器端直接拼接上 Bearer 的前缀
+      token: "Bearer " + tokenStr,
+    });
+  });
 };
